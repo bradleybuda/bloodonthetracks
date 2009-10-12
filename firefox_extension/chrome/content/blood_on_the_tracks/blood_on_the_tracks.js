7c29553cb2e1b38ab5050cd81f6b82d42438be3f
Bradley Buda <brad@tracksimple.com>
2009-10-12 11:58:15 -0700

    seperate into panel and module, print request id to console

--- firefox_extension/chrome/content/blood_on_the_tracks/blood_on_the_tracks.js
+++ firefox_extension/chrome/content/blood_on_the_tracks/blood_on_the_tracks.js
@@ -24,39 +24,44 @@ FBL.ns(function() { with (FBL) {
     }
   };
 
+  // Module
+  Firebug.BloodOnTheTracks = extend(Firebug.Module, {
+    initialize: function () {
+      httpRequestObserver.register();
+    },
+    shutdown: function() {
+      httpRequestObserver.unregister();
+    },
+    showPanel: function(browser, panel) {
+      // TODO check to see which panel this is
+      if (panel.name != "BloodOnTheTracks")
+        return;
+
+      // Check the response headers for the current page to see if
+      // this is a Rails app running the BOTT plugin
+      var requestId = httpRequestObserver.lastRequestId;
+
+      // TODO show nothing if undefined
+      // TODO should we be pulling this from Firefox, or from Firebug?
+      FirebugContext.getPanel("BloodOnTheTracks").printLine(requestId);
+    }
+  });
+
+  // Panel
   function BloodOnTheTracksPanel() {}
   BloodOnTheTracksPanel.prototype = extend(Firebug.Panel, {
     name: "BloodOnTheTracks",
     title: "Rails",
-
-
-    initialize: function() {
-      Firebug.Panel.initialize.apply(this, arguments);
-
-      httpRequestObserver.register();
-
-      Firebug.Console.log("Rails initialized");
-      Firebug.Console.log(this);
-    },
-
-    show: function() {
-      Firebug.Console.log("Rails tab visible");
-      // Check the response headers for the current page to see if
-      // this is a Rails app running the BOTT plugin
-
-      Firebug.Console.log(httpRequestObserver.lastRequestId);
-    },
-
-    shutdown: function() {
-      Firebug.Panel.shutdown.apply(this, arguments);
-
-      Firebug.Console.log("Rails shutdown");
-
-      httpRequestObserver.unregister();
+    searchable: false,
+    editable: false,
+    printLine: function(message) {
+      var elt = this.document.createElement("p");
+      elt.innerHTML = message;
+      this.panelNode.appendChild(elt);
     }
-    
   });
 
+  Firebug.registerModule(Firebug.BloodOnTheTracks);
   Firebug.registerPanel(BloodOnTheTracksPanel);
 
 }});
