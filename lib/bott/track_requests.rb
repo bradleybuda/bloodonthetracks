1bf0d5f58b3dbc7ce0f913b953128706f6b2dc03
Bradley Buda <brad@tracksimple.com>
2009-10-12 09:48:28 -0700

    insert a unique request id

--- lib/bott/track_requests.rb
+++ lib/bott/track_requests.rb
@@ -1,22 +1,22 @@
 require 'pp'
 
+# THIS IS NOT THREAD-SAFE!!
 module BOTT
 class TrackRequests
+  
   def initialize(app)
-    STDERR.puts "BOTT::TrackRequests init"
     @app = app
   end
 
   def call(env)
-    STDERR.puts "BOTT::TrackRequests call"
-    # is this for thread safety?
-    dup._call(env)
-  end
-  
-  def _call(env)
+    @@request_id = rand(2 ** 64).to_s
+    
+    STDERR.puts "BOTT::TrackRequests call, request_id = #{@@request_id}"
     @status, @headers, @response = @app.call(env)
+
+    @headers['X-BOTT-Request-Id'] = @@request_id
+    
     [@status, @headers, @response]
   end
-
 end
 end
