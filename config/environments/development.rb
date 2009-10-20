d187d7813bcb77566f3ecaf286a45bfc19f6037a
Bradley Buda <brad@tracksimple.com>
2009-10-20 15:27:26 -0700

    Move code out of environment.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,38 +16,4 @@ config.action_controller.perform_caching             = false
 # Don't care if the mailer can't send
 config.action_mailer.raise_delivery_errors = false
 
-# Hook up blood_on_the_tracks
-config.middleware.insert_after(ActionController::Failsafe, BOTT::TrackRequests)
-
 # Spy on all controller actions
-require 'pp'
-module ::ActionController
-  class Dispatcher
-    BOTT_DEFAULT_INSTANCE_VARS = ["@_current_render", "@template", "@assigns_added", "@view_paths", "@_first_render", "@output_buffer", "@assigns", "@helpers", "@cached_content_for_layout", "@template_format", "@controller", "@content_for_layout", "@_request"]
-    
-    def bott_after_request
-      STDERR.puts "intercepting request"
-      
-      original_request = @env['action_controller.rescue.request']
-      original_response = @env['action_controller.rescue.response']
-      view = original_response.template
-      instance_variables = {}
-      instance_variables_pretty = {}
-      
-      (view.instance_variables - BOTT_DEFAULT_INSTANCE_VARS).each do |ivar_name|
-        instance_variables[ivar_name] = view.instance_variable_get(ivar_name).dup
-        instance_variables_pretty[ivar_name] = view.instance_variable_get(ivar_name).pretty_inspect
-      end
-      
-      state = BOTT::RequestState.instance
-      
-      state.add_metadata 'controller', original_request.params[:controller]
-      state.add_metadata 'action', original_request.params[:action]
-      state.add_metadata 'params', original_request.params
-      state.add_metadata 'instance_variables', instance_variables
-      state.add_metadata 'instance_variables_pretty', instance_variables_pretty
-    end
-    
-    after_dispatch :bott_after_request
-  end
-end
