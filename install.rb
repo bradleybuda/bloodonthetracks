a01b734b3bba382394e72f4065a65f2f0d7b3526
Bradley Buda <brad@tracksimple.com>
2009-10-20 17:28:01 -0700

    add install script to bundle XPI

---
+++ install.rb
@@ -0,0 +1,22 @@
+STDERR.puts
+STDERR.puts "Building Firefox Extension..."
+
+Dir.chdir 'firefox_extension'
+Dir.chdir 'chrome'
+`zip -r blood_on_the_tracks.jar content/*` # TODO also zip skin dir when we have one
+Dir.chdir '..'
+`zip blood_on_the_tracks.xpi install.rdf chrome.manifest chrome/blood_on_the_tracks.jar`
+`rm chrome/blood_on_the_tracks.jar`
+
+STDERR.puts
+STDERR.puts <<MSG
+Blood on the Tracks successfully installed
+To install the Firefox extension, visit
+http://localhost:3000/blood_on_the_tracks/install
+
+For more info, see http://github.com/bradleybuda/bloodonthetracks
+
+Thanks!
+MSG
+STDERR.puts
+
