007aea5774e21e9ca279cccd9d67391df581f909
Bradley Buda <brad@tracksimple.com>
2009-10-21 11:11:57 -0700

    hard-coded to only run in development mode for now, just to be extra safe

--- init.rb
+++ init.rb
@@ -1,35 +1,36 @@
-require 'pp'
-require 'blood_on_the_tracks/request_state'
+# hacked in for now
+if RAILS_ENV == 'development'
+  require 'pp'
+  require 'blood_on_the_tracks/request_state'
 
-# Tell Dispatcher to call our code to track requests
-# TODO externalize
-module ::ActionController
-  class Dispatcher
-    BOTT_DEFAULT_INSTANCE_VARS = ["@_current_render", "@template", "@assigns_added", "@view_paths", "@_first_render", "@output_buffer", "@assigns", "@helpers", "@cached_content_for_layout", "@template_format", "@controller", "@content_for_layout", "@_request"]
+  # Tell Dispatcher to call our code to track requests
+  # TODO externalize
+  module ::ActionController
+    class Dispatcher
+      BOTT_DEFAULT_INSTANCE_VARS = ["@_current_render", "@template", "@assigns_added", "@view_paths", "@_first_render", "@output_buffer", "@assigns", "@helpers", "@cached_content_for_layout", "@template_format", "@controller", "@content_for_layout", "@_request"]
     
-    def bott_after_request
-      original_request = @env['action_controller.rescue.request']
-      original_response = @env['action_controller.rescue.response']
-      view = original_response.template
-      instance_variables = {}
-      instance_variables_pretty = {}
+      def bott_after_request
+        original_request = @env['action_controller.rescue.request']
+        original_response = @env['action_controller.rescue.response']
+        view = original_response.template
+        instance_variables = {}
+        instance_variables_pretty = {}
       
-      (view.instance_variables - BOTT_DEFAULT_INSTANCE_VARS).each do |ivar_name|
-        instance_variables[ivar_name] = view.instance_variable_get(ivar_name).dup rescue nil
-        instance_variables_pretty[ivar_name] = view.instance_variable_get(ivar_name).pretty_inspect
+        (view.instance_variables - BOTT_DEFAULT_INSTANCE_VARS).each do |ivar_name|
+          instance_variables[ivar_name] = view.instance_variable_get(ivar_name).dup rescue nil
+          instance_variables_pretty[ivar_name] = view.instance_variable_get(ivar_name).pretty_inspect
+        end
+      
+        state = ::BloodOnTheTracks::RequestState.instance
+       
+        state.add_metadata 'controller', original_request.params[:controller]
+        state.add_metadata 'action', original_request.params[:action]
+        state.add_metadata 'params', original_request.params
+        state.add_metadata 'instance_variables', instance_variables
+        state.add_metadata 'instance_variables_pretty', instance_variables_pretty
       end
-      
-      state = ::BloodOnTheTracks::RequestState.instance
-      
-      state.add_metadata 'controller', original_request.params[:controller]
-      state.add_metadata 'action', original_request.params[:action]
-      state.add_metadata 'params', original_request.params
-      state.add_metadata 'instance_variables', instance_variables
-      state.add_metadata 'instance_variables_pretty', instance_variables_pretty
-    end
     
-    after_dispatch :bott_after_request
+      after_dispatch :bott_after_request
+    end
   end
 end
-
-
